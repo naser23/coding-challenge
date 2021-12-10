a43076ae43d3b7bcf103a6e851ba4376006a668e
Naser Abdulkadir <naserabdulkadir0@gmail.com>
2021-12-10 18:20:26 -0500

    first commit. This repo will be used whenever I want to do coding challenges or want to learn a new concept.

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// it wants us to return the first point in the array whose index is equal to its value
+// const solve = (intArray) => {
+//   for (const arr of intArray) {
+//     let goal = arr.filter((value) => {
+//       return value === 1;
+//     });
+
+//     let newArr = [];
+//     for (const x of goal) {
+//       newArr.push(x);
+//       return newArr;
+//     }
+//   }
+// };
+
+// solve([
+//   [1, 4, 2, 1],
+//   [6, 3, 8, 9],
+//   [1, 5, 1, 0],
+// ]);
+
+// const solve = (intArray, k) => {
+//   for (const x of intArray) {
+//     let num = x / k;
+//     console.log(num);
+//   }
+//   return;
+// };
+
+// solve([12, 14, 36, 18], 9);
